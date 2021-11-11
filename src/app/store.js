f189f06036700fc88d1cfc9107e77469a1619893
Tejas Sheth <tejas.sheth@vodafone.com>
2021-11-11 19:36:29 +0530

    bug: imported combine reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,5 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
 import {
 	persistStore,
 	persistReducer,
